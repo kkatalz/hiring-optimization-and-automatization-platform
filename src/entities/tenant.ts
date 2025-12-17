7b2f6a2757d049dc3967a9e0595ae691f5996d38
kkatalz <zlata.kiev2004@gmail.com>
2025-12-17 15:49:42 +0200

    fix: replace increment with uuid

--- src/entities/tenant.ts
+++ src/entities/tenant.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'tenants' })
 export class Tenant {
-  @PrimaryGeneratedColumn('increment')
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
   @Column()
