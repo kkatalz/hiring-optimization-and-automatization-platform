ba7142041b69a57dedcc35e99cb634c13aa9575b
kkatalz <zlata.kiev2004@gmail.com>
2025-12-24 16:26:47 +0200

    feat: add separate methods for creating 4 types of users. Add role decorator to each method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,26 +7,58 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UseInterceptors,
 } from '@nestjs/common';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserRole } from 'src/entities/role.enum';
+import { TenantLogicInterceptor } from 'src/interceptors/tenantId.interceptor';
 import { CreateUserDto } from 'src/user/dto/createUser.dto';
 import { UpdateUserDto } from 'src/user/dto/updateUser.dto';
 import { UserResponseDto } from 'src/user/dto/userResponse.dto';
 import { UserService } from 'src/user/user.service';
 
 @Controller('users')
+@UseInterceptors(TenantLogicInterceptor)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
-    return this.userService.create(createUserDto);
+  @Post('candidate')
+  createCandidate(
+    @Body() createCandidateDto: CreateUserDto,
+  ): Promise<UserResponseDto> {
+    return this.userService.create(createCandidateDto, UserRole.candidate);
   }
 
+  @Roles(UserRole.admin, UserRole.superAdmin)
+  @Post('recruiter')
+  createRecruiter(
+    @Body() createRecruiterDto: CreateUserDto,
+  ): Promise<UserResponseDto> {
+    return this.userService.create(createRecruiterDto, UserRole.recruiter);
+  }
+
+  @Roles(UserRole.superAdmin)
+  @Post('admin')
+  createAdmin(@Body() createAdminDto: CreateUserDto): Promise<UserResponseDto> {
+    return this.userService.create(createAdminDto, UserRole.admin);
+  }
+
+  @Roles(UserRole.superAdmin)
+  @Post('superAdmin')
+  createSuperAdmin(
+    @Body() createSuperAdminDto: CreateUserDto,
+  ): Promise<UserResponseDto> {
+    return this.userService.create(createSuperAdminDto, UserRole.superAdmin);
+  }
+
+  // TODO
+  // @Roles(UserRole.superAdmin)
   @Get()
   findAll(): Promise<UserResponseDto[]> {
     return this.userService.findAll();
   }
 
+  // @Roles(UserRole.superAdmin, UserRole.admin) //admin only for users within his tenant
   @Get('tenant/:id')
   findAllByTenantId(
     @Param('id', new ParseUUIDPipe()) tenantId: string,
@@ -34,6 +66,7 @@ export class UserController {
     return this.userService.findAllByTenantId(tenantId);
   }
 
+  // @Roles(UserRole.superAdmin, UserRole.admin) //admin only for users within his tenant
   @Get(':id')
   findById(
     @Param('id', new ParseUUIDPipe()) id: string,
@@ -41,6 +74,7 @@ export class UserController {
     return this.userService.findDtoById(id);
   }
 
+  // @Roles(UserRole.superAdmin, UserRole.admin) //admin only for users within his tenant
   @Patch(':userId/tenant/:tenantId')
   update(
     @Param('userId', new ParseUUIDPipe()) userId: string,
@@ -50,6 +84,7 @@ export class UserController {
     return this.userService.update(userId, tenantId, updateUserDto);
   }
 
+  // @Roles(UserRole.superAdmin, UserRole.admin) //admin only for users within his tenant
   @Delete(':userId/tenant/:tenantId')
   remove(
     @Param('userId', new ParseUUIDPipe()) userId: string,
