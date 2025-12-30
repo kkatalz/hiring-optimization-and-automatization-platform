3c3a50f8467685f3e58747f2bf5fa12650585dd6
kkatalz <zlata.kiev2004@gmail.com>
2025-12-30 22:54:28 +0200

    feat: test createUser, findDtoById

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -9,7 +9,10 @@ import {
 } from '../../test/database-setup';
 import { User } from '../entities/user';
 import { expect } from 'chai';
-import { testUsers } from '../../test/fixtures/testUsers';
+import {
+  EXPECTED_ACTIVE_USERS,
+  testUsers,
+} from '../../test/fixtures/testUsers';
 import { UserRole } from '../entities/role.enum';
 import { testTenants } from '../../test/fixtures/testTenants';
 import { Tenant } from '../entities/tenant';
@@ -18,6 +21,7 @@ import { AuthModule } from '../auth/auth.module';
 
 describe('UserService', () => {
   let service: UserService;
+  const nonExistentUUIDId = '00000000-0000-0000-0000-000000000000';
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -44,14 +48,8 @@ describe('UserService', () => {
     expect(!!service).to.deep.equal(true);
   });
 
-  it('should find all users', async () => {
-    const allUsers = await service.findAll();
-
-    expect(allUsers.length).to.deep.equal(1);
-  });
-
   describe('create', () => {
-    it('should create user', async () => {
+    it('should create user superAdmin', async () => {
       const tenantId = testTenants[0].id;
 
       const createUserDto: CreateUserDto = {
@@ -61,15 +59,20 @@ describe('UserService', () => {
         lastName: 'Doe',
         tenantId,
       };
-      const role = UserRole.superAdmin;
-      const result = await service.create(createUserDto, role);
+      const targetRole = UserRole.superAdmin;
+      const result = await service.create(createUserDto, targetRole);
 
       expect(result.email).to.equal(createUserDto.email);
       expect(result.tenantId).to.equal(tenantId);
+      expect(result.role).to.equal(targetRole);
       expect(result).to.not.have.property('password');
+      expect(result).to.have.property('token');
+
+      const allUsers = await service.findAll();
+      expect(allUsers.length).to.equal(EXPECTED_ACTIVE_USERS + 1);
     });
 
-    it('should throw if tenantId on user create does not exist', async () => {
+    it('should throw if tenantId in createUserDto does not exist', async () => {
       const dto: CreateUserDto = {
         email: 'noTenant@gmail.com',
         password: 'password',
@@ -82,17 +85,18 @@ describe('UserService', () => {
 
       try {
         await service.create(dto, role);
+        expect.fail('Should have thrown a BAD_REQUEST error but did not');
       } catch (e: any) {
         expect(e).to.have.property('status', 400);
         expect(e.response).to.equal('Tenant does not exist.');
       }
     });
 
-    it('should throw if user with email already exists in tenant', async () => {
+    it('should throw if user with provided email in createUserDto already exists in tenants', async () => {
       const tenantId = testTenants[0].id;
 
       const dto: CreateUserDto = {
-        email: 'existing@gmail.com',
+        email: 'test@dot.com',
         password: 'password',
         firstName: 'John',
         lastName: 'Doe',
@@ -101,14 +105,73 @@ describe('UserService', () => {
 
       const role = UserRole.superAdmin;
 
-      await service.create(dto, role);
-
       try {
         await service.create(dto, role);
+        expect.fail('Should have thrown a BAD_REQUEST error but did not');
       } catch (e: any) {
         expect(e).to.have.property('status', 400);
-        expect(e.response).to.equal('User with given email already exists.');
+        expect(e.response).to.be.equal('User with given email already exists.');
       }
     });
   });
+
+  describe('findDtoById', () => {
+    it('should return dto by user id', async () => {
+      const findUserRecruiter = testUsers[1];
+      const requesterAdmin = testUsers[0];
+
+      const userDtoResult = await service.findDtoById(
+        findUserRecruiter.id,
+        requesterAdmin,
+      );
+
+      expect(userDtoResult.id).to.be.equal(findUserRecruiter.id);
+      expect(userDtoResult.role).to.be.equal(findUserRecruiter.role);
+      expect(userDtoResult.tenantId).to.be.deep.equal(
+        findUserRecruiter.tenantId,
+      );
+      expect(userDtoResult).to.have.property('token');
+    });
+
+    it('should throw if user with given id not found', async () => {
+      const requesterAdmin = testUsers[0];
+
+      try {
+        await service.findDtoById(nonExistentUUIDId, requesterAdmin);
+
+        expect.fail('Should have thrown a NOT_FOUND error but did not');
+      } catch (e) {
+        expect(e.response).to.deep.equal('User with given id not found.');
+      }
+    });
+
+    it('should throw access forbidden if requesters (admins) tenantId differs from searched users tenantId', async () => {
+      const findUserRecruiterWithDifferentTenant = testUsers[3].id;
+      const requesterAdmin = testUsers[0];
+
+      try {
+        await service.findDtoById(
+          findUserRecruiterWithDifferentTenant,
+          requesterAdmin,
+        );
+
+        expect.fail(
+          'Should have thrown a ForbiddenException exception but did not',
+        );
+      } catch (e) {
+        expect(e.response.message).to.deep.equal(
+          'You can access users only within your own tenant.',
+        );
+      }
+    });
+  });
+
+  it('should find all users', async () => {
+    const allUsers = await service.findAll();
+
+    expect(allUsers.length).to.deep.equal(EXPECTED_ACTIVE_USERS);
+  });
 });
+
+// add check that user (superadmin) can not remove himself
+// password can be changed by super and admin. not user himself
