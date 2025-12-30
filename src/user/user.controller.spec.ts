ee3962c60419a893dce3613812bc22902a86759e
kkatalz <zlata.kiev2004@gmail.com>
2025-12-30 21:30:40 +0200

    fix: update missing tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../user/user.controller';
+import { expect } from 'chai';
+import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  cleanDatabase,
+  loadDatabase,
+  testDatabaseConfig,
+} from '../../test/database-setup';
+import { User } from '../entities/user';
+import { Tenant } from '../entities/tenant';
+import { AuthService } from '../auth/auth.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(testDatabaseConfig),
+        TypeOrmModule.forFeature([User, Tenant]),
+      ],
       controllers: [UserController],
+      providers: [UserService, AuthService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    await loadDatabase({});
   });
+  afterEach(() => cleanDatabase());
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(!!controller).to.deep.equal(true);
   });
 });
