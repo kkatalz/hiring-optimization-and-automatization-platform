608a22582755bec1569a09a5809fd6e3769816be
kkatalz <zlata.kiev2004@gmail.com>
2026-01-25 12:50:15 +0200

    feat: turn off eslint rule '/no-require-imports', update depencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,6 +34,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
 
       '@typescript-eslint/no-floating-promises': 'warn',
       'prettier/prettier': [
