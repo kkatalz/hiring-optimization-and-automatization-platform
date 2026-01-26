cb88449f74b97c624fb1550ac6f782ff5bc040a8
kkatalz <zlata.kiev2004@gmail.com>
2026-01-26 17:07:01 +0200

    fix: update tenantId to Not Null in migration

--- src/migrations/1769164356291-AddTenantIdToInterviewAndVacancySubmission.ts
+++ src/migrations/1769164356291-AddTenantIdToInterviewAndVacancySubmission.ts
@@ -13,9 +13,21 @@ export class AddTenantIdToInterviewAndVacancySubmission1769164356291
     await queryRunner.query(
       `ALTER TABLE "vacancy_submissions" ADD "tenant_id" uuid NULL`,
     );
+
+    await queryRunner.query(
+      `UPDATE "vacancy_submissions" SET "tenant_id" = '00000000-0000-0000-0000-000000000000' WHERE "tenant_id" IS NULL`,
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "vacancy_submissions" ALTER COLUMN "tenant_id" SET NOT NULL`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "vacancy_submissions" ALTER COLUMN "tenant_id" DROP NOT NULL`,
+    );
+
     await queryRunner.query(
       `ALTER TABLE "vacancy_submissions" DROP COLUMN "tenant_id"`,
     );
