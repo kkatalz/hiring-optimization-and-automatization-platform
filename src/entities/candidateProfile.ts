fa3b79a30b64baf7785bed507fd0740779dd3c13
kkatalz <zlata.kiev2004@gmail.com>
2026-01-28 00:04:41 +0200

    feat: add candidateProfile entity, set relation with use, add new fields to Vacancy

---
+++ src/entities/candidateProfile.ts
@@ -0,0 +1,24 @@
+import { User } from './user';
+import { LanguageProficiency } from './hiring.enum';
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'candidate_profiles' })
+export class CandidateProfile {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'years_of_experience', type: 'int', nullable: false })
+  yearsOfExperience: number;
+
+  @Column({ type: 'text', nullable: false })
+  country: string;
+
+  @Column({ type: 'text', nullable: false })
+  city: string;
+
+  @Column({ type: 'jsonb', nullable: false })
+  languages: LanguageProficiency[];
+
+  @OneToOne(() => User, (user) => user.candidateProfile)
+  user: User;
+}
