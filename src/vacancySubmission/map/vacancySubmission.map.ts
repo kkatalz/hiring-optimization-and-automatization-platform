46c323c38ca28401f61be5d9704390d2ecdb76e3
kkatalz <zlata.kiev2004@gmail.com>
2026-02-13 17:48:47 +0200

    feat: add candidateProfile to vacancySubmToVacancySubmDto

--- src/vacancySubmission/map/vacancySubmission.map.ts
+++ src/vacancySubmission/map/vacancySubmission.map.ts
@@ -1,5 +1,6 @@
 import { VacancySubmission } from '../../entities/vacancySubmission';
 import { VacancySubmissionDto } from '../../vacancySubmission/dto/vacancySubmission.dto';
+import { candidateToCandidateProfileDto } from '../../candidateProfile/candidate-profile/map/candidate.map';
 
 export const vacancySubmToVacancySubmDto = ({
   id,
@@ -9,6 +10,7 @@ export const vacancySubmToVacancySubmDto = ({
   candidateId,
   status,
   tags,
+  candidateProfile,
 }: VacancySubmission): VacancySubmissionDto => {
   return {
     id,
@@ -18,5 +20,8 @@ export const vacancySubmToVacancySubmDto = ({
     candidateId,
     status,
     tags,
+    candidateProfile: candidateProfile
+      ? candidateToCandidateProfileDto(candidateProfile)
+      : undefined,
   };
 };
