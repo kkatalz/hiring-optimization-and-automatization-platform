0f69511137308a7fca1358cad0349e186289cb1b
kkatalz <zlata.kiev2004@gmail.com>
2026-02-13 12:27:32 +0200

    fix: merge two migrations into one

--- src/migrations/1770928126805-AddUserIdToProfile.ts
+++ src/migrations/1770928126805-AddUserIdToProfile.ts
@@ -13,9 +13,23 @@ export class AddUserIdToProfile1770928126805 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "candidate_profiles" ADD CONSTRAINT "FK_5a3673f11918bcea56f48549603" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
+
+    await queryRunner.query(
+      `ALTER TABLE "vacancy_submissions" DROP CONSTRAINT "FK_d9a6d39fbd85fb4e1e331c84ed4"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "vacancy_submissions" ADD CONSTRAINT "FK_d9a6d39fbd85fb4e1e331c84ed4" FOREIGN KEY ("candidate_id") REFERENCES "candidate_profiles"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "vacancy_submissions" DROP CONSTRAINT "FK_d9a6d39fbd85fb4e1e331c84ed4"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "vacancy_submissions" ADD CONSTRAINT "FK_d9a6d39fbd85fb4e1e331c84ed4" FOREIGN KEY ("candidate_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+
     await queryRunner.query(
       `ALTER TABLE "candidate_profiles" DROP CONSTRAINT "FK_5a3673f11918bcea56f48549603"`,
     );
