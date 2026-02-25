678b63c756c4c0314cbfa8e3d24ca06ec6a5f1a0
kkatalz <zlata.kiev2004@gmail.com>
2026-02-25 13:57:54 +0200

    fix: remove mistakenly added strings

--- src/vacancy/dto/createVacancyQuesion.dto.ts
+++ src/vacancy/dto/createVacancyQuesion.dto.ts
@@ -1,8 +1,4 @@
-@ -1 +1,4 @@
-// Legacy shim file kept due to the misspelled filename `createVacancyQuesion.dto.ts`.
-// TODO: Implement the real DTO in `createVacancyQuestion.dto.ts` and update all imports
-// to use that file directly, then remove this shim.
-export * from './createVacancyQuestion.dto';
+import { IsBoolean, IsNotEmpty } from 'class-validator';
 import { ConvertStringToBool } from '../../decorators/convertStringToBool.decorator';
 
 export class CreateVacancyQuestionDto {
