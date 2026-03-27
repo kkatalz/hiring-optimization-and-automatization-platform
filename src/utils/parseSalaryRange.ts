bb93755068434aecb0627dd778c533eee2593e1b
kkatalz <zlata.kiev2004@gmail.com>
2026-03-28 01:03:59 +0200

    fix: change comment so it's not deceiving

--- src/utils/parseSalaryRange.ts
+++ src/utils/parseSalaryRange.ts
@@ -1,8 +1,8 @@
 import { SalaryRange } from '../types/salaryRange.interface';
 
 /**
- * If salary is a range like "50k-70k" or "50,000 - 70,000", extract min and max as numbers.
- * If it's a single value like "60k", treat it as min=max=60,000.
+ * If salary is a range like "50_000-70_000", extract min and max as numbers.
+ * If it's a single value like "60_000", treat it as min=max=60,000.
  */
 export function parseSalaryRange(salary?: string): SalaryRange | null {
   if (!salary) return null;
