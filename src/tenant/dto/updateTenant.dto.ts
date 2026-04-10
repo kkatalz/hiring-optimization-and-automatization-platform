9d8ccf9fc9a63f01752dde2ab8eeae29ab43f946
kkatalz <zlata.kiev2004@gmail.com>
2026-04-10 23:24:16 +0300

    fix: add @IsNotEmpty() to UpdateTenantDto - the field can be absent (not provided at all), but if provided, it   cannot be "" or null

--- src/tenant/dto/updateTenant.dto.ts
+++ src/tenant/dto/updateTenant.dto.ts
@@ -1,11 +1,13 @@
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateTenantDto {
   @IsEmail()
+  @IsNotEmpty()
   @IsOptional()
   readonly email?: string;
 
   @IsString()
+  @IsNotEmpty()
   @IsOptional()
   readonly slug?: string;
 }
