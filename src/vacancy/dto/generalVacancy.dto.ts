eab3cdb69af8dd9d3d26266926b66102c79ec50c
kkatalz <zlata.kiev2004@gmail.com>
2026-04-14 11:59:40 +0300

    refactor: clean DTOs - remove decorators from ouput DTOs

--- src/vacancy/dto/generalVacancy.dto.ts
+++ src/vacancy/dto/generalVacancy.dto.ts
@@ -1,61 +1,16 @@
-import {
-  IsArray,
-  IsEnum,
-  IsInt,
-  IsNumber,
-  IsOptional,
-  IsString,
-  ValidateNested,
-} from 'class-validator';
-import {
-  LanguageProficiency,
-  TimeCommitment,
-} from '../../entities/hiring.enum';
-import { Type } from 'class-transformer';
+import { LanguageProficiency, TimeCommitment } from '../../entities/hiring.enum';
 import { GeneralVacancyQuestionDto } from './generalVacancyQuestion.dto';
 
 export class GeneralVacancyDto {
   id: string;
-
-  @IsString()
   name: string;
-
-  @IsString()
   description: string;
-
-  @IsOptional()
-  @IsString()
   salary?: string;
-
-  @IsNumber()
-  @IsOptional()
   numberOfSubmissions?: number;
-
-  @IsOptional()
-  @IsEnum(TimeCommitment)
   timeCommitment?: TimeCommitment;
-
-  @IsOptional()
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => LanguageProficiency)
   languageRequirements?: LanguageProficiency[];
-
-  @IsOptional()
-  @IsInt()
   requiredYearsOfExperience?: number;
-
-  @IsOptional()
-  @IsArray()
-  @IsString({ each: true })
   tags?: string[];
-
-  @IsOptional()
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => GeneralVacancyQuestionDto)
   vacancyQuestions?: GeneralVacancyQuestionDto[];
-
-  @IsOptional()
   createdAt?: Date;
 }
