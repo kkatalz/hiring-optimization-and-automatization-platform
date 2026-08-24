fbb106d27742f2a17e7ffe54aa2f416caad60d3d
kkatalz <zlata.kiev2004@gmail.com>
2026-08-24 23:15:01 +0300

    feat: style error nicely

--- frontend/src/components/vacancies/VacanciesList.tsx
+++ frontend/src/components/vacancies/VacanciesList.tsx
@@ -1,13 +1,14 @@
 import { List, Pagination } from '@mui/material';
-import type { ReactNode } from 'react';
+import { useState, type ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import { setPage } from '@/features/filters/vacancyFiltersSlice';
 import { getErrorMessage } from '@/utils/errorMessage';
 import VacancyCard from './VacancyCard';
-import type { PaginatedResponse, VacancySummary } from '@/types';
+import type { Notification, PaginatedResponse, VacancySummary } from '@/types';
 import type { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import type { SerializedError } from '@reduxjs/toolkit';
+import NotificationAlert from '@/components/common/NotificationAlert';
 
 interface Props<VacancyExtended extends VacancySummary> {
   data?: PaginatedResponse<VacancyExtended>;
@@ -27,13 +28,17 @@ export const VacanciesList = <VacancyExtended extends VacancySummary>({
 }: Props<VacancyExtended>) => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const [notification, setNotification] = useState<Notification | null>(null);
 
   const appliedFilters = useAppSelector((state) => state.vacancyFilters);
 
   const currentPage = appliedFilters.page ?? 1;
 
   if (isError)
-    return <div>Could not load vacancies - {getErrorMessage(error)}</div>;
+    setNotification({
+      message: `Could not load vacancies - ${getErrorMessage(error)}. Try refreshing the page or contact support if the problem persists.`,
+      severity: 'error',
+    });
 
   return (
     <List
@@ -45,6 +50,11 @@ export const VacanciesList = <VacancyExtended extends VacancySummary>({
         gap: 1,
       }}
     >
+      <NotificationAlert
+        notification={notification}
+        onClose={() => setNotification(null)}
+      />
+
       {data?.data.map((vacancy, index) => (
         <VacancyCard
           key={vacancy.id}
