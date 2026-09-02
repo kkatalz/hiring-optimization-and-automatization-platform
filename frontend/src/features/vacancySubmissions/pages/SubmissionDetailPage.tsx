1f6ad4d79e9adc95d31efdf7ad253636dde5019d
kkatalz <zlata.kiev2004@gmail.com>
2026-09-02 14:48:11 +0300

    feat: start SubmissionDetailPage - add CandidateInfo,  LanguagesChips and ApplicationStatusWorkflow

--- frontend/src/features/vacancySubmissions/pages/SubmissionDetailPage.tsx
+++ frontend/src/features/vacancySubmissions/pages/SubmissionDetailPage.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import { useFindSubmissionByIdQuery } from '@/features/vacancySubmissions/api/vacancySubmissionEndpoints';
+import { ApplicationStatusWorkflow } from '@/features/vacancySubmissions/components/ApplicationStatusWorkflow';
+import CandidateInfo from '@/features/vacancySubmissions/components/CandidateInfo';
+import LanguagesChips from '@/shared/ui/LanguagesChips';
+import { Alert, Card, CardContent, Grid } from '@mui/material';
+import { useParams } from 'react-router-dom';
 
 const SubmissionDetailPage = () => {
-  return <div>SubmissionDetailPage</div>;
+  const submissionId = useParams().submissionId as string;
+
+  const {
+    data: submission,
+    isLoading,
+    isError,
+  } = useFindSubmissionByIdQuery(submissionId);
+
+  const candidateProfile = submission?.candidateProfile;
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError || !submission || !candidateProfile)
+    return (
+      <Alert severity='error'>
+        Could not load vacancy submission. Try refreshing the page or contact
+        support if the problem persists.
+      </Alert>
+    );
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid size={4}>
+          <Card>
+            <CardContent
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              {candidateProfile && (
+                <>
+                  <CandidateInfo
+                    candidateProfile={candidateProfile}
+                    globalDirection='column'
+                    showChipOrAvatar={'avatar'}
+                  />
+                  <LanguagesChips languages={candidateProfile.languages} />
+                </>
+              )}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid size={8}>
+          <Card>
+            <CardContent>
+              <ApplicationStatusWorkflow submissionStatus={submission.status} />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default SubmissionDetailPage;
